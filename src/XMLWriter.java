c9e6a5d97204deadd021040cc7de512a20fd3eb3
David Parker <davidgparker123@gmail.com>
2014-10-08 10:55:27 -0500

    Removed extra space

--- src/XMLWriter.java
+++ src/XMLWriter.java
@@ -267,7 +267,7 @@ public class XMLWriter {
 		writeControlDataType(c,al);
 		
 		write(closeTag("DataObject"));
-		write(createTag("Description",null,"Specifies ... Valid Range:\n Default Value:\n ",true));
+		write(createTag("Description",null,"Specifies ... Valid Range:\nDefault Value:\n ",true));
 		write(createTag("Unit",null,"",true));
 		
 		/* Booleans need a range table */
